4729363571363ea517f40ed3734646da8cf72172
perfectcharm <57660209+perfectcharm@users.noreply.github.com>
2021-03-05 02:01:13 +0300

    Adding table output methods

--- main.go
+++ main.go
@@ -3,35 +3,38 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"log"
+	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
+
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+// struct model Todo of db
 type Todo struct {
-	task           string    `json:"task"`
-	datetime_start time.Time `json:"datetime_start"`
-	datetime_end   time.Time `json:"datetime_end"`
+	Task           string `json:"task"`
+	Datetime_start string `json:"datetime_start"`
+	Datetime_end   string `json:"datetime_end"`
 }
 
-var database *sql.DB
-var todo []Todo
-
+// function show all tasks
 func ShowTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var todo []Todo
 
-	result, err := database.Query("SELECT task, datetime_start, datetime_end FROM todo")
+	result, err := db.Query("SELECT * FROM todo")
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
 	defer result.Close()
 
 	for result.Next() {
 		var tasks Todo
-		err := result.Scan(&tasks.task, &tasks.datetime_start, &tasks.datetime_end)
+
+		err := result.Scan(&tasks.Task, &tasks.Datetime_start, &tasks.Datetime_end)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -40,35 +43,39 @@ func ShowTask(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(todo)
 }
 
+// show one task on a given time range in URL
 func OneTask(w http.ResponseWriter, r *http.Request) {
-	rows, err := database.Query("SELECT * FROM todo")
+	w.Header().Set("Content-Type", "application/json")
+	var todo Todo
+	dateOne := r.URL.Query().Get("dateOne")
+	dateTwo := r.URL.Query().Get("dateTwo")
+	// read value from URL. Example: localhost:8000/todo/one?dateOne='2021-03-02'&dateTwo='2021-03-03'
+	result := db.QueryRow(`SELECT datetime_start, datetime_end FROM todo 
+						WHERE datetime_start BETWEEN $1 and $2 OR datetime_end BETWEEN $1 and $2`, dateOne, dateTwo)
+	err := result.Scan(&todo.Datetime_start, &todo.Datetime_end)
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
-	defer rows.Close()
+	json.NewEncoder(w).Encode(todo)
 }
 
 func AddTask(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func DeleteTask(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func main() {
-	connect := "user=postgres password=poMSta dbname=task sslmode=disable"
-	db, err := sql.Open("postgres", connect) // Connect to DB
+	var err error
+
+	db, err = sql.Open("postgres", fmt.Sprintf("host=localhost port=5432 user=postgres password=poMSta dbname=task sslmode=disable"))
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
 	defer db.Close()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/todo", ShowTask).Methods("GET")
-	router.HandleFunc("/todo/{datatime}", OneTask).Methods("GET")
+	router.HandleFunc("/todo/one", OneTask).Methods("GET")
 	router.HandleFunc("/todo/add", AddTask).Methods("POST")
-	router.HandleFunc("/todo/delete/{}", DeleteTask).Methods("DELETE")
 
 	http.ListenAndServe(":8000", router)
 }
